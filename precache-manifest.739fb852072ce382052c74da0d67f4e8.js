83bd06add62a8d20f4593bb7c4c06169f57f0cb9
intelligentiallc <intelligentiallc@gmail.com>
2020-05-26 19:19:58 -0400

    Updates

---
+++ precache-manifest.739fb852072ce382052c74da0d67f4e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a317ac984a2ce69af66889a7d579c190",
+    "url": "/dashboard-1/index.html"
+  },
+  {
+    "revision": "618cd12197185cc872d4",
+    "url": "/dashboard-1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "78fe5325d96b298fed95",
+    "url": "/dashboard-1/static/js/2.bd57b5c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dashboard-1/static/js/2.bd57b5c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "618cd12197185cc872d4",
+    "url": "/dashboard-1/static/js/main.90f29ba1.chunk.js"
+  },
+  {
+    "revision": "86f7be514988b061a17f",
+    "url": "/dashboard-1/static/js/runtime-main.2648755f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dashboard-1/static/media/logo.5d5d9eef.svg"
+  }
+]);
