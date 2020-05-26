7dc223e3ebbba60d694dabc86592244d169daf43
intelligentiallc <intelligentiallc@gmail.com>
2020-05-26 19:09:03 -0400

    init

---
+++ src/containers/App/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+export default function App() {
+  return (
+    <Router>
+        <Switch>
+          {/* <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route> */}
+        </Switch>
+    </Router>
+  );
+}
+
+
